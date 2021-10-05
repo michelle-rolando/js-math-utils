62e60b8148a00e152ba50527f699c3e8c0a883c8
michelle-rolando <michellerolandokanno@gmail.com>
2021-10-04 20:02:28 -0500

    Función suma

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ function showNumber (number){
 console.log("Holi mundo");
 
 console.log("tercer commit");
+
+// suma 
+
+let suma = function(a,b){
+    console.log("El resultado de la suma es: " + a+b);
+}
+
+// fin de suma
